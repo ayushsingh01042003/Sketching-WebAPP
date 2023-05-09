798b4c0f41f819cb80d2e79126d508ebc580e6a6
Ayush <ayushsingh01042003@gmail.com>
2023-05-10 00:20:15 +0530

    structure the JS file

--- script.js
+++ script.js
@@ -8,31 +8,34 @@ const colorBtn = document.getElementById('colorButton');
 const eraseBtn = document.getElementById("eraseButton");
 const clearBtn = document.getElementById("clearButton");
 const colorPicker = document.getElementById("colorPicker");
+const grid = document.getElementsByClassName("grid");
 
 sliderValue.addEventListener("input", () => {
     sliderText.innerHTML = `${sliderValue.value} X ${sliderValue.value}`;
 });
 
-colorBtn.addEventListener("click", () => {
-    colorBtn.style.backgroundColor = '#aba3d7';
-    eraseBtn.style.backgroundColor = '';
-    clearBtn.style.backgroundColor = '';
 
-    //functionality of the color button
-    //first get the current color seleted in the color selector
-    //then get the current size from the slider 
-    //when the mouse is moved on the drawing grid and clicked anywhere on it 
-    //A pixes of the color and size should be made on it
-});
+function setButtonActive(button, otherButton1, otherButton2) {
+    button.style.backgroundColor = '#aba3d7';
+    otherButton1.style.backgroundColor = '';
+    otherButton2.style.backgroundColor = '';
+}
 
-eraseBtn.addEventListener("click", () => {
-    eraseBtn.style.backgroundColor = '#aba3d7';
-    colorBtn.style.backgroundColor = '';
-    clearBtn.style.backgroundColor = '';
-});
+function colorMode() {
+    colorBtn.addEventListener("click", () => {
+        setButtonActive(colorBtn, eraseBtn, clearBtn);
+    });
+}
 
-clearBtn.addEventListener("click", () => {
-    clearBtn.style.backgroundColor = '#aba3d7';
-    colorBtn.style.backgroundColor = '';
-    eraseBtn.style.backgroundColor = '';
-});
+function eraseMode() {
+    eraseBtn.addEventListener("click", () => {
+        setButtonActive(eraseBtn, colorBtn, clearBtn);
+    });
+}
+
+function clear() {
+    clearBtn.addEventListener("click", () => {
+        setButtonActive(clearBtn, colorBtn, eraseBtn);
+        // clear the grid
+    });
+}
