b7a2193dc3b856c45d9e6fc0b7547a082d79170d
Ayush <ayushsingh01042003@gmail.com>
2023-05-10 00:56:04 +0530

    Revert "Add javascript for slider text and buttons color"
    This reverts commit 811a42ec20917d51ef01082cc36054c38b43290b.

--- script.js
+++ script.js
@@ -8,34 +8,31 @@ const colorBtn = document.getElementById('colorButton');
 const eraseBtn = document.getElementById("eraseButton");
 const clearBtn = document.getElementById("clearButton");
 const colorPicker = document.getElementById("colorPicker");
-const grid = document.getElementsByClassName("grid");
 
 sliderValue.addEventListener("input", () => {
     sliderText.innerHTML = `${sliderValue.value} X ${sliderValue.value}`;
 });
 
+colorBtn.addEventListener("click", () => {
+    colorBtn.style.backgroundColor = '#aba3d7';
+    eraseBtn.style.backgroundColor = '';
+    clearBtn.style.backgroundColor = '';
 
-function setButtonActive(button, otherButton1, otherButton2) {
-    button.style.backgroundColor = '#aba3d7';
-    otherButton1.style.backgroundColor = '';
-    otherButton2.style.backgroundColor = '';
-}
+    //functionality of the color button
+    //first get the current color seleted in the color selector
+    //then get the current size from the slider 
+    //when the mouse is moved on the drawing grid and clicked anywhere on it 
+    //A pixes of the color and size should be made on it
+});
 
-function colorMode() {
-    colorBtn.addEventListener("click", () => {
-        setButtonActive(colorBtn, eraseBtn, clearBtn);
-    });
-}
+eraseBtn.addEventListener("click", () => {
+    eraseBtn.style.backgroundColor = '#aba3d7';
+    colorBtn.style.backgroundColor = '';
+    clearBtn.style.backgroundColor = '';
+});
 
-function eraseMode() {
-    eraseBtn.addEventListener("click", () => {
-        setButtonActive(eraseBtn, colorBtn, clearBtn);
-    });
-}
-
-function clear() {
-    clearBtn.addEventListener("click", () => {
-        setButtonActive(clearBtn, colorBtn, eraseBtn);
-        // clear the grid
-    });
-}
+clearBtn.addEventListener("click", () => {
+    clearBtn.style.backgroundColor = '#aba3d7';
+    colorBtn.style.backgroundColor = '';
+    eraseBtn.style.backgroundColor = '';
+});
