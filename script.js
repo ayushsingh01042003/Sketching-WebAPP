d91e1e4992b299e52e2da59386673a8e13d64695
Ayush <ayushsingh01042003@gmail.com>
2023-05-21 23:22:07 +0530

    phase 2

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-const DEFAULT_COLOR = '#333333'
-const DEFAULT_SIZE = 16
-
-let currentColor = DEFAULT_COLOR;
-let currentSize = DEFAULT_SIZE;
-
 const sliderText = document.getElementById("sliderText");
 const sliderValue = document.getElementById("sliderValue");
 const colorBtn = document.getElementById('colorButton');
@@ -11,61 +5,72 @@ const eraseBtn = document.getElementById("eraseButton");
 const clearBtn = document.getElementById("clearButton");
 const colorPicker = document.getElementById("colorPicker");
 const grid = document.getElementById('grid');
+const canvas = document.querySelector(`canvas`);
 
-function defaultMode() {
-    currentColor = DEFAULT_COLOR;
-    currentSize = DEFAULT_SIZE;
-    colorBtn.click();
+const DEFAULT_COLOR = '#333333';
+const DEFAULT_SIZE = 16;
+let currentColor = DEFAULT_COLOR;
+let currentSize = DEFAULT_SIZE;
+
+const colorMode = () => {
+    colorBtn.style.backgroundColor = '#dfb3b0c4';
+    clearBtn.style.backgroundColor = '';
+    eraseBtn.style.backgroundColor = '';
+
+    //code for coloring on the blank space
+    //1. Load the grid
+    loadGrid();
+    //2. Call the coloring function
+    startColoring();
+
 }
 
-colorBtn.addEventListener("click", () => {
-    colorBtn.style.backgroundColor = '#aba3d7';
-    eraseBtn.style.backgroundColor = '';
-    clearBtn.style.backgroundColor = '';
+const loadGrid = () => {
+    const gridSize = DEFAULT_SIZE; 
+    const gridWidth = grid.clientWidth; 
+    const cellSize = gridWidth / gridSize; 
 
-    // code for coloring the grid
+    // Clear the existing grid
+    grid.innerHTML = '';
+  
+    // Create grid cells and append them to the grid container
+    for (let i = 0; i < gridSize; i++) {
+      for (let j = 0; j < gridSize; j++) {
+        const cell = document.createElement('div');
+        cell.classList.add('grid-cell');
+        cell.style.width = `${cellSize}px`;
+        cell.style.height = `${cellSize}px`;
+        cell.style.border = '1px solid blue'
+        grid.appendChild(cell);
+      }
+    }
+} 
+
+const startColoring = () => {
     currentColor = colorPicker.value;
     currentSize = sliderValue.value;
+    
 
-    grid.addEventListener("mousedown", drawPixel);
-});
+}
 
-eraseBtn.addEventListener("click", () => {
-    eraseBtn.style.backgroundColor = '#aba3d7';
+const eraseMode = () => {
+    eraseBtn.style.backgroundColor = '#dfb3b0c4';
     colorBtn.style.backgroundColor = '';
     clearBtn.style.backgroundColor = '';
-    
-    // remove drawing listener and add erase listener
-    grid.removeEventListener("mousedown", drawPixel);
-    grid.addEventListener("mousedown", erasePixel);
-});
 
-sliderValue.addEventListener("input", () => {
-    sliderText.innerHTML = `${sliderValue.value} X ${sliderValue.value}`;
-});
+}
 
 clearBtn.addEventListener("click", () => {
-    clearBtn.style.backgroundColor = '#aba3d7';
+    clearBtn.style.backgroundColor = '#dfb3b0c4';
     colorBtn.style.backgroundColor = '';
     eraseBtn.style.backgroundColor = '';
-    
-    // remove drawing listener and add clear listener
-    grid.removeEventListener("mousedown", drawPixel);
-    grid.addEventListener("mousedown", clearPixel);
-    grid.innerHTML = '';
+
+    //clear the coloring area
+})
+
+sliderValue.addEventListener('input', () => {
+    sliderText.textContent = `${sliderValue.value} X ${sliderValue.value}`;
 });
 
-  
-function erasePixel(event) {
-    if (event.target !== grid) {
-        event.target.remove();
-    }
-}
-
-function clearPixel(event) {
-    grid.innerHTML = '';
-}
-
-window.onload = () => {
-    defaultMode();
-}
+colorBtn.addEventListener(`click`, colorMode);
+eraseBtn.addEventListener(`click`, eraseMode);
